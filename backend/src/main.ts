bb93843b391a80682ab69fdd5122be05b7b38fb1
Camilo Alejandro Soto Vega <olimpusmac@gmail.com>
2025-06-25 09:52:19 -0500

    chore(back-docs): setup Swagger for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend - Prueba Técnica Camilo Soto')
+    .setDescription('API para la gestión de proyectos, tareas y usuarios')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: 'api/json',
+  });
 
   app.useGlobalPipes(new ValidationPipe({}));
 
