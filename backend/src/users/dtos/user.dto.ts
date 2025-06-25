801ca12ed59fb2b591da8966c6db0c3c098a7676
Camilo Alejandro Soto Vega <olimpusmac@gmail.com>
2025-06-25 10:00:05 -0500

    fix(back): user dto validations

--- backend/src/users/dtos/user.dto.ts
+++ backend/src/users/dtos/user.dto.ts
@@ -1,39 +1,43 @@
 import { ApiProperty, ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import {
-    IsEmail,
-    IsEnum,
-    IsOptional,
-    IsString,
-    MinLength,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
 } from 'class-validator';
 
 export class UserDto {
-    @ApiProperty({ description: 'nombre completo' })
-    @IsString()
-    name: string;
+  @ApiProperty({ description: 'nombre completo' })
+  @IsString({ message: 'Nombre debe ser un texto' })
+  @IsNotEmpty({ message: 'El nombre no puede estar vacio' })
+  name: string;
 
-    @ApiProperty({ description: 'correo electronico' })
-    @IsEmail()
-    email: string;
+  @ApiProperty({ description: 'correo electronico' })
+  @IsEmail({}, { message: 'Correo electronico no valido' })
+  email: string;
 
-    @ApiProperty({ description: 'Contraseña (minimo 6 caracteres)' })
-    @IsString()
-    @MinLength(6)
-    password: string;
+  @ApiProperty({ description: 'Contraseña (minimo 6 caracteres)' })
+  @IsString()
+  @MinLength(6, { message: 'la contraseña debe tener al menos 6 caracteres' })
+  password: string;
 
-    @ApiPropertyOptional({
-        description: 'Cargo',
-        enum: ['admin', 'manager', 'developer'],
-        default: 'developer',
-    })
-    @IsOptional()
-    @IsEnum(['admin', 'manager', 'developer'])
-    role?: 'admin' | 'manager' | 'developer';
+  @ApiPropertyOptional({
+    description: 'Cargo',
+    enum: ['admin', 'manager', 'developer'],
+    default: 'developer',
+  })
+  @IsOptional()
+  @IsEnum(['admin', 'manager', 'developer'], {
+    message: 'El cargo debe ser admin, manager o developer',
+  })
+  role?: 'admin' | 'manager' | 'developer';
 
-    @ApiPropertyOptional({ description: 'URL de la imagen' })
-    @IsOptional()
-    @IsString()
-    avatar?: string;
+  @ApiPropertyOptional({ description: 'URL de la imagen' })
+  @IsOptional()
+  @IsString()
+  avatar?: string;
 }
 
 export class UpdateUserDto extends PartialType(UserDto) {}
