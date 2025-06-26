e30fe73a135b5597969a2509fe4569ae36415665
Camilo Alejandro Soto Vega <olimpusmac@gmail.com>
2025-06-26 11:54:30 -0500

    feat(back): post and put user only admin

--- backend/src/users/controllers/user.controller.ts
+++ backend/src/users/controllers/user.controller.ts
@@ -1,16 +1,30 @@
-import {Body, Controller, Get, Param, Post, Put, UseGuards} from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 import { UserDto, UpdateUserDto } from '../dtos/user.dto';
 import { UserService } from '../services/user.service';
-import {AuthGuard} from "../../auth/guards/auth.guard";
+import { AuthGuard } from '../../auth/guards/auth.guard';
+
+import { Roles } from '../../common/decorators/roles.decorator';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { Role } from '../../common/enums/role.enum';
 
 @Controller('users')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
+@ApiBearerAuth()
 export class UserController {
   constructor(private readonly service: UserService) {}
 
   @Post()
+  @Roles(Role.Admin)
   @ApiOperation({ summary: 'Crear desarrollador' })
   create(@Body() body: UserDto) {
     return this.service.create(body);
@@ -23,6 +37,7 @@ export class UserController {
   }
 
   @Put(':id')
+  @Roles(Role.Admin)
   @ApiOperation({ summary: 'Actualizar desarrollador' })
   update(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.service.update(id, body);
